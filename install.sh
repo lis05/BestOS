290a17bb6cc7337555359ab2aac5a7ce63490147
Igor Lukyanov <lis05st@gmail.com>
2023-02-16 21:34:27 +0200

    added twm

--- install.sh
+++ install.sh
@@ -2,13 +2,34 @@
 
 # this script completelly installs BestOS to a freshly installed minimal Arch Linux system
 
-# PAMAC
-sudo pacman -S --needed git base-devel
+#! PAMAC
+sudo pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/libpamac-aur.git
 cd libpamac-aur
-makepkg -si
+makepkg -si -- noconfirm
 cd ..
 git clone https://aur.archlinux.org/pamac-aur.git
 cd pamac-aur
-makepkg -si
+makepkg -si --noconfirm
 cd ..
+
+sudo pacman -S --noconfirm --needed xorg-server                     # Xorg
+sudo pacman -S --noconfirm --needed xf86-video-amdgpu               # video drivers
+sudo pacman -S --noconfirm --needed mesa                            # OpenGL
+sudo pacman -S --noconfirm --needed lib32-mesa                      # OpenGL  
+
+sudo pacman -S --noconfirm --needed sddm                            # display manager
+sudo systemctl enable sddm
+
+sudo pacman -S --noconfirm --needed qtile                           # Qtile
+sudo mkdir /etc/sddm.conf.d
+sudo cp ../files/sddm-config /etc/sddm.conf.d/default.conf
+
+sudo pacman -S --noconfirm --needed xterm                           # xterm
+sudo pacman -S --noconfirm --needed terminology                     # terminology
+
+echo "DONE! Reboot to see the changes"
+
+
+
+
