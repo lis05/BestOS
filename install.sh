c0b5f1b78d2018b371ee3104ce4b8372c9d19514
Igor Lukyanov <lis05st@gmail.com>
2023-02-16 20:23:08 +0200

    +pamac

--- install.sh
+++ install.sh
@@ -2,5 +2,13 @@
 
 # this script completelly installs BestOS to a freshly installed minimal Arch Linux system
 
-# GIT
-sudo pacman --noconfirm -S git 
+# PAMAC
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/libpamac-aur.git
+cd libpamac-aur
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/pamac-aur.git
+cd pamac-aur
+makepkg -si
+cd ..
