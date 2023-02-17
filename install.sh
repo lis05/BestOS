a6743ddb55645e800ef49a2ddfa7197ba2c0c349
Igor Lukyanov <lis05st@gmail.com>
2023-02-17 20:08:19 +0200

    x

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ cd pamac-aur
 makepkg -si --noconfirm
 cd ..
 
+#! basic software
+
 sudo pacman -S --noconfirm --needed xorg-server                     # Xorg
 sudo pacman -S --noconfirm --needed xf86-video-amdgpu               # video drivers
 sudo pacman -S --noconfirm --needed mesa                            # OpenGL
@@ -25,12 +27,15 @@ sudo pacman -S --noconfirm --needed qtile                           # Qtile
 sudo mkdir /etc/sddm.conf.d
 sudo cp files/sddm-config /etc/sddm.conf.d/default.conf
 
+#! software
 sudo pacman -S --noconfirm --needed xterm
 sudo pacman -S --noconfirm --needed terminology
 sudo pacman -S --noconfirm --needed firefox
-sudo pacman -S --noconfirm --needed telegram-dekstop                           
+sudo pacman -S --noconfirm --needed telegram-dekstop      
 
-echo "DONE! Reboot to see the changes"
+sudo pacman -S --noconfirm --needed lolcat                           
+
+echo "DONE! BestOS has been installed, reboot now to see the changes" | lolcat
 
 
 
