e717896d802115b819a3c1ce8126bb832bd504e0
Igor Lukyanov <lis05st@gmail.com>
2023-02-24 20:40:49 +0200

    123

--- files/software/rgbw-cursors/install.sh
+++ files/software/rgbw-cursors/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rsync -r ./ $HOME/.icons/rgbw-cursors/
+mkdir -p $HOME/.icons/default
+mkdir -p $HOME/.config/gtk-3.0
 cp default-index.theme $HOME/.icons/default/index.theme
 cp gtk3-settings.ini ~/.config/gtk-3.0/settings.ini
